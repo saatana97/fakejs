f43ce1a241f75c29fedfc47c953261455ab89290
向文可 <1041367524@qq.com>
2022-11-16 17:43:44 +0800

    fix: image error

--- example/index.cjs
+++ example/index.cjs
@@ -1,4 +1,3 @@
 const R = require("../dist/fakejs.umd.cjs");
 console.info("nickname", R.nickname());
 console.info("word", R.word({ length: 1000, number: true, repeatable: false }));
-console.info("image", R.image());
--- src/modules/image.ts
+++ src/modules/image.ts
@@ -1,7 +1,7 @@
 import { number } from "..";
 
 /**
- * 生成指定尺寸随机颜色图片
+ * 生成指定尺寸随机颜色图片，目前仅支持浏览器环境
  * @param width 图片宽度
  * @param height 图片年高度
  * @param color 指定图片颜色
@@ -12,28 +12,22 @@ const fn = (
     height: number = 100,
     color?: number
 ): string => {
-    if (!global.document) {
-        throw new Error("目前仅支持在浏览器环境中生成图片");
-    }
     const cvs = document.createElement("canvas");
     cvs.width = width;
     cvs.height = height;
     const ctx = cvs.getContext("2d");
     if (typeof color === "undefined") {
-        color = number(0, 0xfff);
+        color = number(0, 0xffffff);
     }
     if (ctx) {
-        ctx.fillStyle = color + "";
+        ctx.fillStyle = "#" + new Number(color).toString(16);
         ctx.fillRect(0, 0, width, height);
-        ctx.fillStyle = 0xfff - color + "";
+        ctx.fillStyle = "#" + new Number(0xffffff - color).toString(16);
         const text = `${width} × ${height}`;
-        const fontSize = Math.min((width / text.length) * 0.5, height * 0.8);
-        ctx.font = fontSize + "px";
-        ctx.fillText(
-            text,
-            (width - fontSize * text.length) / 2,
-            (height - fontSize) / 2
-        );
+        const fontSize = Math.min((width / text.length) * 1.5, height * 0.8);
+        ctx.textAlign = "center";
+        ctx.font = fontSize + "px 黑体";
+        ctx.fillText(text, width / 2, height / 2);
     }
     return cvs.toDataURL();
 };
